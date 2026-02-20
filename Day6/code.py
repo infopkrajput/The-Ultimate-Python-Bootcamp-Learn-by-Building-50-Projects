d81dec8f1a9632d1ae843e96e24ebd937fc74694
infopkrajput <info.pkrajput@gmail.com>
2026-02-20 17:44:11 +0530

    complete day6

---
+++ Day6/code.py
@@ -0,0 +1,56 @@
+"""
+Challenge: Daily Learning Journal Logger
+
+Build a Python script that allows you to maintain a daily learning journal. Each entry will be saved into a `.txt` file along with a timestamp.
+
+Your program should:
+1. Ask the user what they learned today.
+2. Add the entry to a file called `learning_journal.txt`.
+3. Each entry should include the date and time it was written.
+4. The journal should **append** new entries rather than overwrite.
+
+Bonus:
+- Add an optional rating (1-5) for how productive the day was.
+- Show a confirmation message after saving the entry.
+- Make sure the format is clean and easy to read when opening the file.
+
+Example:
+📅 2025-06-14 — 10:45 AM
+Today I learned about how list comprehensions work in Python!
+Productivity Rating: 4/5
+"""
+
+import datetime
+
+star = "*" * 50
+
+print(star)
+print("Welcome to Daily Learning Journal Logger")
+print(f"**** Today {datetime.datetime.now().strftime('%d-%m-%Y')} ****")
+
+while True:
+    learn = input("What do you learn today?: ")
+    rate = input("Rate your todays learning (1-5): ")
+    confirmation = input("Do you really want to save this journal in file? (yes/no): ")
+    if confirmation == "yes":
+        try:
+            file = open("learning_journal.txt", "a")
+            file.write(f"📅 {datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')}\n")
+            file.write(f"Today I learned about {learn}!\n")
+            file.write(f"Productivity Rating: {rate}/5\n\n")
+        finally:
+            file.close()
+            print("Entry saved successfully!")
+    else:
+        print("See you later!")
+    
+    exit_or_not = input("Wanna close journal book? (yes/no)")
+    if exit_or_not.lower() == "yes":
+        break
+        
+        
+            
+            
+
+
+
