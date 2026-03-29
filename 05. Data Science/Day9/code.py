1b64527a6aa02cbb5a832fcad34f7a01db28a330
infopkrajput <info.pkrajput@gmail.com>
2026-03-29 23:54:44 +0530

    finish 05. Data Science Day9

--- 05. Data Science/Day9/code.py
+++ 05. Data Science/Day9/code.py
@@ -1,12 +1,21 @@
-{
-    "title": "The 7 Habits of Highly Effective People",
-    "author": "Stephen R. Covey",
-    "genre": "Self-help",
-    "description": "A guide to personal and professional effectiveness through proven principles."
-}
-
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 df = pd.read_csv('books.csv')
+
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(df['description'])
+
+cosine_sin = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+indices = pd.Series(df.index, index=df['title'])
+
+def get_recommendations(title, cosine_sin=cosine_sin):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sin[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:6]
+    book_indices = [i[0] for i in sim_scores]
+    return df[['title', 'author']].iloc[book_indices]
+
+print(get_recommendations('The Great Gatsby'))
